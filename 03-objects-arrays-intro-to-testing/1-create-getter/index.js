a49bc08334986c6046304edc27f1df48d5fa1140
Каляев Александр Сергеевич <C:\Program Files\The Bat!\Mail>
2024-09-26 15:17:43 +0300

    3rd task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    path = path.split('.')
+    return (obj) => {
+        let current = {}
+        Object.assign(current, obj)
+        current.__proto__ = null
+        path.forEach((elem) => {
+            if (current == undefined) {
+                return
+            }
+            current = current[elem]
+        })
+        return current
+    }       
 }
