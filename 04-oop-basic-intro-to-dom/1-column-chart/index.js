8d581223fb8d5b23914b4a6d95a6125d4e094691
Каляев Александр Сергеевич <C:\Program Files\The Bat!\Mail>
2024-10-12 17:27:35 +0300

    5.1 task

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -6,17 +6,17 @@ export default class ColumnChart {
   constructor(props = {}) {
     const {
       data = [],
-        label = '',
-        link = '',
-        value = 0,
-        formatHeading = (value) => value,
-    } = props
+      label = '',
+      link = '',
+      value = 0,
+      formatHeading = (value) => value,
+    } = props;
     this.data = data,
-      this.label = label,
-      this.link = link,
-      this.value = value,
-      this.formatHeading = formatHeading,
-      this.createCharts()
+    this.label = label,
+    this.link = link,
+    this.value = value,
+    this.formatHeading = formatHeading,
+    this.createCharts();
   }
 
   createTemplate() {
@@ -33,54 +33,54 @@ export default class ColumnChart {
             ${this.createColumnsTemplate(this.data)}
           </div>
         </div>
-      </div>`
+      </div>`;
   }
 
   createColumnsTemplate(data) {
-    const maxValue = Math.max(...data)
+    const maxValue = Math.max(...data);
     return data.map(i => {
-      const scale = this.chartHeight / maxValue
-      const percent = (i / maxValue * 100).toFixed(0)
-      return `<div style="--value: ${Math.floor(i * scale)}" data-tooltip="${percent}%"></div>`
-    }).join('')
+      const scale = this.chartHeight / maxValue;
+      const percent = (i / maxValue * 100).toFixed(0);
+      return `<div style="--value: ${Math.floor(i * scale)}" data-tooltip="${percent}%"></div>`;
+    }).join('');
   }
 
   getLink() {
     if (this.link) {
-      return `<a class="column-chart__link" href="${this.link}">View all</a>`
+      return `<a class="column-chart__link" href="${this.link}">View all</a>`;
     } else {
-      return ''
+      return '';
     }
   }
 
   createCharts() {
-    const element = document.createElement('div')
-    element.innerHTML = this.createTemplate()
-    this.element = element.firstElementChild
+    const element = document.createElement('div');
+    element.innerHTML = this.createTemplate();
+    this.element = element.firstElementChild;
     if (this.data.length) {
-      this.element.classList.remove('column-chart_loading')
+      this.element.classList.remove('column-chart_loading');
     }
-    this.updateElements = this.getSubElements(this.element)
+    this.updateElements = this.getSubElements(this.element);
   }
 
-    getSubElements(element) {
-      const elements = element.querySelectorAll('[data-element="body"]')
-      let result = {}
-      for (const i of Array.from(elements)) {
-          result[i.dataset.element] = element
-      }
-      return result
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element="body"]');
+    let result = {};
+    for (const i of Array.from(elements)) {
+      result[i.dataset.element] = element;
     }
+    return result;
+  }
 
   update(data) {
-    this.updateElements.body.innerHTML = this.createColumnsTemplate(data)
+    this.updateElements.body.innerHTML = this.createColumnsTemplate(data);
   }
 
   remove() {
-    this.element.remove()
+    this.element.remove();
   }
 
   destroy() {
-    this.remove()
+    this.remove();
   }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,52 @@
 export default class SortableTable {
   constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig
+    this.data = data
+    this.render()
+  }
 
+  createHeaderTemplate() {
+    return `    
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        ${this.headerConfig.map((elem) => `    
+            <div class="sortable-table__cell" data-id="${elem.id}" data-sortable="${elem.sortable}" data-order="">
+              <span>${elem.title}</span>
+            </div>      
+          `).join('')}  
+      </div>`
+  }
+
+  createTableTemplate() {
+    return `
+      ${this.data.map((item) =>
+        `<a href="/products/${item.id}" class="sortable-table__row">
+        ${this.headerConfig.map((elem) => {
+          if (elem['template']) {
+            return elem['template'](item[elem['id']])
+          }
+          return `<div class="sortable-table__cell">${item[elem['id']]}</div>`
+          }).join('')
+        } </a>`).join('')
+      }`
+  }
+
+  createElement(html) {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    return div;
+  }
+
+
+  render() {
+    const elemHtmlCode = this.createElement(`
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          ${this.createHeaderTemplate()}
+          <div data-element="body" class="sortable-table__body">
+            ${this.createTableTemplate()}
+          </div>
+        </div>
+      </div>`).firstElementChild;
+    this.element = elemHtmlCode;
   }
 }
-
